64ecac43ccf4dac90a4c627f9751a241bcfb5bc7
Aychew M <134740817+aychew1229@users.noreply.github.com>
2026-04-19 12:16:57 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// This is the service worker (sw.js)
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // This allows the app to be "installable"
-  event.respondWith(fetch(event.request));
-});
