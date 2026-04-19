5ebc1620e879f2f4d92cb06eda80b2a88dc8d923
Aychew M <134740817+aychew1229@users.noreply.github.com>
2026-04-19 15:19:41 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'aychew-portfolio-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './profile1.jpg',
+  // External Google Fonts
+  'https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Mono:ital,wght@0,300;0,400;1,300&family=Fraunces:ital,wght@0,300;0,600;1,300&display=swap'
+];
+
+// Install Event: Save files to the browser cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event: Serve files from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached file, or try to get it from the network
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
